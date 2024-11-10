455e1c78f313eac7d828c5dffbc55b2b9ebd03b6
Umair Ul Hassan <umairhassan0154@gmail.com>
2024-11-10 19:22:58 +0500

    Add scrolling in login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView, Image, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView, Image, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 const Logo = () => (
   <View style={styles.logoContainer}>
     <Image
-      source={require('../assets/VSARA.png')} // Replace 'logo.png' with your actual logo file name
+      source={require('../assets/VSARA.png')} // Replace with your actual logo file name
       style={styles.logoImage}
     />
   </View>
 );
 
 export default function LoginScreen() {
-  const navigation = useNavigation();  // Use navigation hook
+  const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
@@ -22,61 +22,63 @@ export default function LoginScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Logo />
-      
-      <Text style={styles.title}>Login here</Text>
-      <Text style={styles.subtitle}>Welcome Back</Text>
-      
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoCompleteType="email"
-        />
+      <ScrollView contentContainerStyle={styles.scrollContentContainer}>
+        <Logo />
         
-        <Text style={styles.label}>Password</Text>
-        <View style={styles.passwordContainer}>
+        <Text style={styles.title}>Login here</Text>
+        <Text style={styles.subtitle}>Welcome Back</Text>
+        
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email</Text>
           <TextInput
             style={styles.input}
-            placeholder="Password"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry={!showPassword}
-            autoCompleteType="password"
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCompleteType="email"
           />
-          <TouchableOpacity
-            style={styles.eyeIcon}
-            onPress={togglePasswordVisibility}
-            accessibilityLabel="Toggle password visibility"
-          >
-            <Ionicons
-              name={showPassword ? "eye-off" : "eye"}
-              size={24}
-              color="#999"
+          
+          <Text style={styles.label}>Password</Text>
+          <View style={styles.passwordContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!showPassword}
+              autoCompleteType="password"
             />
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.eyeIcon}
+              onPress={togglePasswordVisibility}
+              accessibilityLabel="Toggle password visibility"
+            >
+              <Ionicons
+                name={showPassword ? "eye-off" : "eye"}
+                size={24}
+                color="#999"
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      
-      <TouchableOpacity style={styles.forgotPasswordContainer}>
-        <Text style={styles.forgotPasswordText}>Forgot your password?</Text>
-      </TouchableOpacity>
-      
-      <TouchableOpacity style={styles.signInButton}>
-        <Text style={styles.signInButtonText}>Sign in</Text>
-      </TouchableOpacity>
-      
-      <TouchableOpacity style={styles.signUpContainer}
-      onPress={() => navigation.navigate('CreateAccountDoctor')}>
-        <Text style={styles.signUpText}>
-          Don't have an account? <Text style={styles.signUpLink}>Sign Up</Text>
-        </Text>
-      </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.forgotPasswordContainer}>
+          <Text style={styles.forgotPasswordText}>Forgot your password?</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.signInButton}>
+          <Text style={styles.signInButtonText}>Sign in</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.signUpContainer}
+        onPress={() => navigation.navigate('CreateAccountDoctor')}>
+          <Text style={styles.signUpText}>
+            Don't have an account? <Text style={styles.signUpLink}>Sign Up</Text>
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -85,8 +87,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  scrollContentContainer: {
     padding: 20,
     justifyContent: 'center',
+    flexGrow: 1,
   },
   logoContainer: {
     flexDirection: 'row',
@@ -99,11 +104,6 @@ const styles = StyleSheet.create({
     height: 120,
     marginRight: 5,
     resizeMode: 'contain',
-  },
-  logoText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
   },
   title: {
     fontSize: 24,
@@ -148,15 +148,6 @@ const styles = StyleSheet.create({
   forgotPasswordText: {
     color: '#0EA5E9',
     fontSize: 14,
-  },
-  nextButton: {
-    backgroundColor: '#0EA5E9',
-    borderRadius: 8,
-    padding: 10,
-    alignItems: 'center',
-    marginTop: 20,
-    alignSelf: 'center',
-    width: 50,
   },
   signInButton: {
     backgroundColor: '#0EA5E9',
